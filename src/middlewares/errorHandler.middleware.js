ab930c03e68e27f0d1044821a70328dbed2624ec
Chirag Rathod <124911968+ChiragRathod25@users.noreply.github.com>
2025-04-24 18:59:02 +0000

    Fix typo in error handler status code property

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -1,7 +1,7 @@
 import { logger } from "../utils/logger.js";
 
 export const errorHandler = (err, req, res, next) => {
-    let statusCode = err.statusCode || 500;
+    let statusCode = err.statuscode || 500;
     let message = err.message || "Internal Server Error";
 
     // Log the error with full details
